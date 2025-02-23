56dbd33edb9b90ee8f642dbc4e1352224244906f
Nathan Bedell <nbedell@tulane.edu>
2025-02-23 12:31:50 -0500

    Begin implementing autocompletion algorithm

--- src/lsp/autocomplete.rs
+++ src/lsp/autocomplete.rs
@@ -2,7 +2,10 @@ use rayon::prelude::*;
 use tower_lsp::lsp_types::Position;
 
 use crate::{
-    ast::{raw_expr::RawExpr, Statement},
+    ast::{
+        raw_expr::{RawExpr, RawExprRec},
+        Statement,
+    },
     debugging::Debugger,
     interpreter::{Context, VariableId},
     parser::{parse, CodeLocation},
@@ -18,9 +21,55 @@ pub fn autocomplete_at<'a, T: Debugger + 'static>(
     statements: &Vec<Statement<VariableId>>,
     cursor: Position,
 ) -> Vec<String> {
-    // TODO: Traverse through the statements until arriving at the correct line,
-    // then traverse through the statement to try to find the correct column.
-    // In the future we may want to build an index to speed this process up.
+    // Note: In the future we may want to build an index to speed this process up.
+
+    // Do a binary search to try to find a matching line.
+    let search_result = statements
+        .binary_search_by(|x| x.location.unwrap().line.cmp(&cursor.line));
+
+    match search_result {
+        Ok(i) => {
+            // If we found a matching line, we can just search the matching
+            // statement.
+            let found_line = &statements[i];
+
+            let found_expr = find_expr_before(cursor, &found_line.expr);
+
+            autocomplete(context, &found_expr)
+        }
+        Err(i) => {
+            // Otherwise, we need to look at the line before where the
+            //  match would have been inserted.
+
+            let search_line = &statements[i - 1];
+
+            // This should be a lambda that we can look into to find the actual line.
+            match &search_line.expr.expr {
+                RawExprRec::Lambda(_, lambda_statements, _) => {
+                    autocomplete_at(context, &lambda_statements, cursor)
+                }
+                _ => panic!("Internal error: Autocompletion expected a lambda"),
+            }
+        }
+    }
+}
+
+///
+/// Traverses through an expression to find the expression immediately before the
+///  cursor's location on the current line.
+///
+/// For example, if there was an expression at (2:31), and another at (2:33)
+///  and the cursor was at (2:32), we'd return the expression at (2:31).
+///
+/// Note: Currently our line / column numbering data does not include the entire
+///  "length" of the tokens, which makes it difficult to tell if the cursor
+///  is actually "after" an expression. This will have to be fixed somehow
+///  in the future.
+///
+fn find_expr_before(
+    cursor: Position,
+    expr: &RawExpr<VariableId>,
+) -> RawExpr<VariableId> {
     todo!()
 }
 
